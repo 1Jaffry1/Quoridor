8e97ed0d06c042da07b147287b2bcff09b30311a
SMJ <muhammadj110@gmail.com>
2022-07-06 18:33:05 +0430

    added tophaspath

--- quoridor/src/Back/Node.java
+++ quoridor/src/Back/Node.java
@@ -3,6 +3,7 @@ package Back;
 public class Node {
     private final int x;
     private final int y;
+    private int player;
 
     public Node(int x, int y) {
         this.x = x;
@@ -24,5 +25,15 @@ public class Node {
 
     public int show() {
         return 10 * x + y;
+    }
+
+    public static boolean tophaspath(Node n) {
+        if (n.player == 1) {
+            if (n.x == Board.getHeight() - 1) {
+                return true;
+            }
+            else return(tophaspath(new Node(n.x, n.y+1)));
+        }
+        return false;
     }
 }
