f6c22f65a54f89f5b8605ef133f9f3a4973b2d09
SMJ <muhammadj110@gmail.com>
2022-07-06 17:42:24 +0430

    added classes Node and Edge

---
+++ quoridor/src/Back/Node.java
@@ -0,0 +1,28 @@
+package Back;
+
+public class Node {
+    private final int x;
+    private final int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return x + "" + y;
+    }
+
+    public int show() {
+        return 10 * x + y;
+    }
+}
