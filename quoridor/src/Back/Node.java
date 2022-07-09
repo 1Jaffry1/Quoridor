5fdca00e96f173389502688e8dba1b092dac139f
SMJ <muhammadj110@gmail.com>
2022-07-09 11:22:39 +0430

    added haspath() and removed tophaspath()

--- quoridor/src/Back/Node.java
+++ quoridor/src/Back/Node.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 public class Node {
     private final int x;
     private final int y;
-    public static ArrayList<Node> nodes = new ArrayList<Node>();
-    public ArrayList<Node> neighbors = new ArrayList<Node>();
-    public ArrayList<Edge> edges = new ArrayList<Edge>();
-    private int playerishere;
-    private int topvalue = 1;
+    public static ArrayList<Node> nodes = new ArrayList<>();
+    public ArrayList<Node> neighbors = new ArrayList<>();
+    public ArrayList<Edge> edges = new ArrayList<>();
+    private boolean playerIsHere;
+    static Node noNode = new Node(-2, -2);
 
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    static Node none = new Node (-1, -1);
+
     public int getX() {
         return x;
     }
@@ -24,13 +24,13 @@ public class Node {
         return y;
     }
 
-    public static Node getNodeByCoordinates(int x, int y){
-        for (Node n : nodes){
-            if (n.getX() == x && n.getY() == y){
+    public static Node getNodeByCoordinates(int x, int y) {
+        for (Node n : nodes) {
+            if (n.getX() == x && n.getY() == y) {
                 return n;
             }
         }
-        return none;
+        return noNode;
     }
 
     @Override
@@ -38,28 +38,50 @@ public class Node {
         return x + "" + y;
     }
 
-    public int show() {
+    public int printPosition() {
         return 10 * y + x;
     }
 
-    public static boolean tophaspath(Node n) {
-        if (n.playerishere == 1) {
-            if (n.y == Board.getHeight() - 1)  //base case
-                return true;
-            return tophaspath(n.moveupwards());
-//            return (tophaspath(new Node(n.x - 1, n.y)));
+    public boolean hasPath(String n) { //temp placeholder
+        if (n.equals("top")) {
+            ArrayList<Node> visited = new ArrayList<>();
+            visited.add(this);
+            for (int i = 0; i < visited.size(); i++) {
+                for (Node neighbor : visited.get(i).neighbors) {
+                    if (!visited.contains(neighbor)) visited.add(neighbor);
+                }
+            }
+            ArrayList<Node> top = new ArrayList<>();
+            for (int x = 0; x < 9; x++) top.add(getNodeByCoordinates(x, 8));
+            for (Node v : top) {
+                if (visited.contains(v)) return true;
+            }
+        }
+        if (n.equals("bottom")) {
+            ArrayList<Node> visited = new ArrayList<>();
+            visited.add(this);
+            for (int i = 0; i < visited.size(); i++) {
+                for (Node neighbor : visited.get(i).neighbors) {
+                    if (!visited.contains(neighbor)) visited.add(neighbor);
+                }
+            }
+            ArrayList<Node> bottom = new ArrayList<>();
+            for (int x = 0; x < 9; x++) bottom.add(getNodeByCoordinates(x, 0));
+            for (Node v : bottom) {
+                if (visited.contains(v)) return true;
+            }
         }
         return false;
     }
 
-    public boolean islegalwalk(Node n) {
+    public boolean isLegalWalk(Node n) {
         for (Node i : this.neighbors) {
             if (i.equals(n)) return true;
         }
         return false;
     }
 
-    public Node moveupwards() {
+    public Node moveUpwards() {
         return new Node(this.x, this.y + 1);
     }
 }
