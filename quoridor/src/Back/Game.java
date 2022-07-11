dfa375b37953135bcc6dbe0e76a986c1d1741248
SMJ <muhammadj110@gmail.com>
2022-07-12 03:01:36 +0430

    added win parameter

--- quoridor/src/Back/Game.java
+++ quoridor/src/Back/Game.java
@@ -6,6 +6,7 @@ public class Game {
     private Board board;
     private Player player1;
     private Player player2;
+    public boolean win = false;
 
     public Game(Board board, Player player1, Player player2) {
         this.board = board;
