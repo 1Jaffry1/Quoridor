543904d699310fe1ab66ad1c865c05c8ba4d3ba9
SMJ <muhammadj110@gmail.com>
2022-07-12 15:59:25 +0430

    removed fx

--- quoridor/src/Back/Board.java
+++ quoridor/src/Back/Board.java
@@ -5,19 +5,12 @@ import java.util.ArrayList;
 public class Board {
     private final int height = 9; //not count ing zero, will be 9 at end. same for width
     private final int width = 9;
-    private final Wall[][] Hwalls = new Wall[height - 2][width - 1];
-    private final Wall[][] Vwalls = new Wall[height - 1][width - 2];
-    private final Node[][] nodes = new Node[height][width];
     public ArrayList<Node> grid;
-    public Player[] players = new Player[2];
 
-
-    public Board() {
-        createCell();
-        createWall();
-        createPlayer();
-        Node.setPlayerIsHere();
+    public Board(ArrayList<Node> main) {
     }
+//    public Player[] players = new Player[2];
+
 
 
     public int getHeight() {
@@ -38,28 +31,4 @@ public class Board {
         }
         return "";
     }
-
-    private void createCell(){
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                nodes[i][j] = new Node(i, j);
-            }
-        }
-    }
-    private void createWall(){
-        //create hwalls
-        for (int i = 0 ; i<height-2; i++){
-            for (int j = 0 ;j< width -1 ;j++)
-                Hwalls[i][j] = new Wall(Node.getNodeByCoordinates(i,j),"h");
-        }
-        //create vwalls
-        for (int i  = 0; i< height - 1; i++)
-            for(int j = 0; j < width -2; j++)
-                Vwalls[i][j] = new Wall(Node.getNodeByCoordinates(i,j),"v");
-    }
-    private void createPlayer(){
-        players[0] = new Player(Node.getNodeByCoordinates(0,8),0);
-        players[1] = new Player(Node.getNodeByCoordinates(8,8),1);
-    }
-
 }
--- quoridor/src/Back/Wall.java
+++ quoridor/src/Back/Wall.java
@@ -1,49 +1,30 @@
 package Back;
 
-import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 
-public class Wall extends Rectangle {
+public class Wall {
     public static ArrayList<Wall> wallsplaced = new ArrayList<>();
     //    default wall:
-    private final Node start;
+    private Node start;
     private final String allign;
-    private final int row;
-    private final int column;
 
     public Wall(Node start, String allign) { // constructor
         this.start = start;
         this.allign = allign;
-//        if (this.islegal()) this.placeWall(); //places wall as soon as it is built after checking that it is legal
+        if (this.islegal()) this.placeWall(); //places wall as soon as it is built after checking that it is legal
     }
 
     public Wall(String string) { // constructor
         String[] split = string.split("");
         this.start = Node.getNodeByCoordinates(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
         this.allign = split[2];
-//        if (this.islegal()) this.placeWall(); //places wall as soon as it is built after checking that it is legal
+        if (this.islegal()) this.placeWall(); //places wall as soon as it is built after checking that it is legal
 
     }
 
-    public Wall(int row, int column,/* double layoutX, double layoutY,*/ String allign) {
-        this.row = row;
-        this.column = column;
-        this.allign = allign;
-        double layoutX = row*10;
-        double layoutY = column*10;
-        if (allign.equals("h")){
-            setHeight(18);
-            setWidth(48);
-        }
-        else if (allign.equals("v")){
-            setHeight(48);
-            setWidth(18);
-        }
-        setLayoutX(layoutX);
-        setLayoutY(layoutY);
-    }
-//
+
+    //
 //    public static Wall getWallByNode(Node start, String allign) {
 //        for (Wall wall : wallsplaced) {
 //            if (wall.equals(new Wall(start, allign))) return wall;
@@ -77,37 +58,29 @@ public class Wall extends Rectangle {
 
         if (allign.equals("h")) {
             Node topnode = Node.getNodeByCoordinates(getStart().getI() - 1, getStart().getJ());
-            if (topnode != null) try {
+            if (topnode != null) {
                 this.start.neighbors.remove(topnode);
                 topnode.neighbors.remove(this.start);
-            } catch (Exception e) {
-                System.out.println("Wall already here");
             }
             Node rightnode = Node.getNodeByCoordinates(getStart().getI(), getStart().getJ() + 1);
             assert rightnode != null;
             Node topnode2 = Node.getNodeByCoordinates(rightnode.getI() - 1, rightnode.getJ());
-            if (topnode2 != null) try {
+            if (topnode2 != null) {
                 rightnode.neighbors.remove(topnode2);
                 topnode2.neighbors.remove(rightnode);
-            } catch (Exception e) {
-                System.out.println("Wall already here");
             }
 
         } else if (allign.equals("v")) {
             Node Rightnode = Node.getNodeByCoordinates(getStart().getI(), getStart().getJ() + 1);
-            if (Rightnode != null) try {
+            if (Rightnode != null) {
                 this.start.neighbors.remove(Rightnode);
                 Rightnode.neighbors.remove(this.start);
-            } catch (Exception e) {
-                System.out.println("Wall already here");
             }
             Node topnode = Node.getNodeByCoordinates(getStart().getI() - 1, getStart().getJ());
             Node Rightnode2 = Node.getNodeByCoordinates(topnode.getI(), topnode.getJ() + 1); //node that is right to topnode
-            if (Rightnode2 != null) try {
+            if (Rightnode2 != null) {
                 topnode.neighbors.remove(Rightnode2);
                 Rightnode2.neighbors.remove(topnode);
-            } catch (Exception e) {
-                System.out.println("Wall already here");
             }
         }
         wallsplaced.add(this);
